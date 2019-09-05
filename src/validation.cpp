8aefe86fcd951537eb0a3f0b14d050786ad01997
dezzyboy <dezzyboy@tradebit.ca>
2019-09-05 00:37:39 -0400

    hard fork

--- src/validation.cpp
+++ src/validation.cpp
@@ -1037,14 +1037,27 @@ bool ReadRawBlockFromDisk(std::vector<uint8_t>& block, const CBlockIndex* pindex
 
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
+    // int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
+    // // Force block reward to zero when right shift is undefined.
+    // if (halvings >= 64)
+    //     return 0;
+
+    CAmount nSubsidy = 50 * COIN;    
     int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
     // Force block reward to zero when right shift is undefined.
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 50 * COIN;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-    nSubsidy >>= halvings;
+    if (nHeight <17291)
+        nSubsidy = 347 * COIN;
+    else
+     {
+	nSubsidy >>= halvings;
+     }    
+
+    
+    // Subsidy is cut in half every 240,000 blocks which will occur approximately every 4 years.
+    
     return nSubsidy;
 }
 
